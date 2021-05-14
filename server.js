8b715dc33b69b7336993a02be8b6bc4cd04265ad
Aniket Kulkarni <32155586+Aniket1299@users.noreply.github.com>
2021-05-14 19:44:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ const isAuthenticated = (req, res, next) => {
 	} else {
 		// Authentication done redirecting to room
 		return res.redirect(
-			`http://localhost:5000/?roomno=${roomno}`
+			`https://syncex.herokuapp.com/?roomno=${roomno}`
 		);
 	}
 };
